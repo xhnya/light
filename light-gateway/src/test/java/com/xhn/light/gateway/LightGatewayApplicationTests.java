35fd1a3a550bac6847fd06ab9cdc5e00fd107ca0
xhn930957853 <930957853@qq.com>
2022-01-02 15:18:15 +0800

    添加依赖，创建gateway

---
+++ light-gateway/src/test/java/com/xhn/light/gateway/LightGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xhn.light.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
