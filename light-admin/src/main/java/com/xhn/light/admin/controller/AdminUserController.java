ffb25015572f1d520081136a2cdb0bea5942f33e
xhn <930957853@qq.com>
2022-01-19 14:17:10 +0800

    模拟登录

--- light-admin/src/main/java/com/xhn/light/admin/controller/AdminUserController.java
+++ light-admin/src/main/java/com/xhn/light/admin/controller/AdminUserController.java
@@ -19,7 +19,14 @@ public class AdminUserController {
     @PostMapping("/login")
     public Result loginAdmin(@RequestParam Map<String,Object> params){
         log.info(String.valueOf(params));
-        return Result.ok();
+        return Result.ok().data("token","admin");
+    }
+    @GetMapping("/info")
+    public Result info(){
+        return Result.ok()
+                .data("roles","[admin]")
+                .data("name","admin")
+                .data("avatar","http://img.xhnya.top/img/vae.jpg");
     }
 
 
