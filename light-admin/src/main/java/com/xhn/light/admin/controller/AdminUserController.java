6a861e5925b6f2858938fc5ecd6e3fd8a876b37b
xhn <930957853@qq.com>
2022-01-19 12:51:26 +0800

    登录

--- light-admin/src/main/java/com/xhn/light/admin/controller/AdminUserController.java
+++ light-admin/src/main/java/com/xhn/light/admin/controller/AdminUserController.java
@@ -1,10 +1,26 @@
 package com.xhn.light.admin.controller;
 
+import com.xhn.light.common.utils.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
 /**
  * @Description: 提供管理后台登录
  * @author: xhn
  * @date: 2022/1/10 17:39
  */
+@Slf4j
+@RequestMapping("admin/user")
+@RestController
 public class AdminUserController {
+
+    @PostMapping("/login")
+    public Result loginAdmin(@RequestParam Map<String,Object> params){
+        log.info(String.valueOf(params));
+        return Result.ok();
+    }
+
 
 }
--- light-common/src/main/java/com/xhn/light/common/exceptionhandler/GlobalExceptionHandler.java
+++ light-common/src/main/java/com/xhn/light/common/exceptionhandler/GlobalExceptionHandler.java
@@ -1,39 +1,39 @@
 package com.xhn.light.common.exceptionhandler;
 
 
-import com.xhn.light.common.utils.Result;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
+//import com.xhn.light.common.utils.Result;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.web.bind.annotation.ControllerAdvice;
+//import org.springframework.web.bind.annotation.ExceptionHandler;
+//import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author xhn
  * @data 2021/1/31 - 9:38
- */
-@ControllerAdvice
-@Slf4j
-public class GlobalExceptionHandler {
-
-    @ExceptionHandler(Exception.class)
-    @ResponseBody
-    public Result error(Exception e){
-        e.printStackTrace();
-        return  Result.error().message("执行了全局异常处理");
-    }
-
-    @ExceptionHandler(ArithmeticException.class)
-    @ResponseBody
-    public Result errorArithmeticException(ArithmeticException e){
-        e.printStackTrace();
-        return  Result.error().message("执行了ArithmeticException异常处理");
-    }
-
-    @ExceptionHandler(LightException.class)
-    @ResponseBody
-    public Result guliException(LightException e){
-        log.error(e.getMessage());
-        e.printStackTrace();
-        return  Result.error().message(e.getMsg()).code(e.getCode());
-    }
-}
+// */
+//@ControllerAdvice
+//@Slf4j
+//public class GlobalExceptionHandler {
+//
+//    @ExceptionHandler(Exception.class)
+//    @ResponseBody
+//    public Result error(Exception e){
+//        e.printStackTrace();
+//        return  Result.error().message("执行了全局异常处理");
+//    }
+//
+//    @ExceptionHandler(ArithmeticException.class)
+//    @ResponseBody
+//    public Result errorArithmeticException(ArithmeticException e){
+//        e.printStackTrace();
+//        return  Result.error().message("执行了ArithmeticException异常处理");
+//    }
+//
+//    @ExceptionHandler(LightException.class)
+//    @ResponseBody
+//    public Result lightException(LightException e){
+//        log.error(e.getMessage());
+//        e.printStackTrace();
+//        return  Result.error().message(e.getMsg()).code(e.getCode());
+//    }
+//}
