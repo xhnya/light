6122f869bf54207e65c86a1ed5757c97f191d7ab
xhn930957853 <930957853@qq.com>
2022-01-31 21:59:55 +0800

    评分

--- light-game/src/main/java/com/xhn/light/game/service/impl/BannerServiceImpl.java
+++ light-game/src/main/java/com/xhn/light/game/service/impl/BannerServiceImpl.java
@@ -1,12 +1,16 @@
 package com.xhn.light.game.service.impl;
 
+import com.xhn.light.common.utils.Constant;
 import com.xhn.light.game.entity.vo.GameAdminBannerVo;
+import com.xhn.light.game.entity.vo.GameBannerInfoListVo;
 import com.xhn.light.game.entity.vo.GameBannerListVo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -17,7 +21,7 @@ import com.xhn.light.game.dao.BannerDao;
 import com.xhn.light.game.entity.BannerEntity;
 import com.xhn.light.game.service.BannerService;
 
-
+@Slf4j
 @Service("bannerService")
 public class BannerServiceImpl extends ServiceImpl<BannerDao, BannerEntity> implements BannerService {
 
@@ -28,7 +32,7 @@ public class BannerServiceImpl extends ServiceImpl<BannerDao, BannerEntity> impl
     public PageUtils queryPage(Map<String, Object> params) {
         IPage<BannerEntity> page = this.page(
                 new Query<BannerEntity>().getPage(params),
-                new QueryWrapper<BannerEntity>().eq(params.get("id")!=null,"game_id",params.get("id"))
+                new QueryWrapper<BannerEntity>().eq(params.get("id") != null, "game_id", params.get("id"))
         );
 
         return new PageUtils(page);
@@ -37,14 +41,34 @@ public class BannerServiceImpl extends ServiceImpl<BannerDao, BannerEntity> impl
     @Override
     public PageUtils getGameBannerList(Map<String, Object> params) {
         IPage<BannerEntity> page = new Query<BannerEntity>().getPage(params);
-
-        List<GameBannerListVo> result=bannerDao.getGameBannerList(page);
-        return new PageUtils(result,result.size(), (int) page.getSize(), (int) page.getCurrent());
+        QueryWrapper<BannerEntity> wrapper = new QueryWrapper<>();
+        wrapper.groupBy("game_id");
+        List<BannerEntity> list = baseMapper.selectList(wrapper);
+        List<GameBannerListVo> result = bannerDao.getGameBannerList(page);
+        log.info(String.valueOf(list.size()));
+        return new PageUtils(result, Math.toIntExact(list.size()), (int) page.getSize(), (int) page.getCurrent());
     }
 
     @Override
     public List<GameAdminBannerVo> getGameBannerUrl(Long id) {
         return bannerDao.getGameBannerUrl(id);
+    }
+
+    @Override
+    public PageUtils getGameBannerUrlByGameId(Map<String, Object> params) {
+        IPage<BannerEntity> page = new Query<BannerEntity>().getPage(params);
+        long gameId=1L;
+        if (params.get("gameId") != null) {
+            gameId=Long.parseLong((String)params.get("gameId"));
+        } else {
+            gameId = 1L;
+        }
+        QueryWrapper<BannerEntity> wrapper = new QueryWrapper<>();
+        wrapper.eq("game_id",gameId);
+        Long count = baseMapper.selectCount(wrapper);
+
+        List<GameBannerInfoListVo> result = bannerDao.getGameBannerUrlByGameId(page, gameId);
+        return new PageUtils(result, Math.toIntExact(count), (int) page.getSize(), (int) page.getCurrent());
     }
 
 }
